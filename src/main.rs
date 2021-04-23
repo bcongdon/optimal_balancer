1178633b1c49f20a0ce143f9c8295cd2e8d391ec
Benjamin <ben@congdon.dev>
2021-04-23 07:13:11 -0700

    Restructure main to not be one long function

--- src/main.rs
+++ src/main.rs
@@ -26,29 +26,29 @@ struct Config {
     funds: Vec<Fund>,
 }
 
+impl Config {
+    fn validate(&self) -> Result<()> {
+        let fund_proportion_sum: f64 = self.funds.iter().map(|f| f.target_proportion).sum();
+        if (fund_proportion_sum - 1.0).abs() > 0.01 {
+            bail!(
+                "expected target_proportions to sum to 1.00, got {:}",
+                fund_proportion_sum
+            );
+        }
+        Ok(())
+    }
+}
+
 fn f64_to_real(ctx: &Context, val: f64) -> Real {
     // NOTE: This is lossy, since we only use 3 decimal digits.
     ast::Real::from_real_str(ctx, &format!("{:.3}", val), "1").unwrap()
 }
 
-fn main() -> Result<()> {
-    let opts: Opts = Opts::parse();
-
-    let config_str = std::fs::read_to_string(opts.config)?;
-
-    let config: Config = toml::from_str(&config_str)?;
-    let funds = config.funds;
-
-    let fund_proportion_sum: f64 = funds.iter().map(|f| f.target_proportion).sum();
-    if (fund_proportion_sum - 1.0).abs() > 0.01 {
-        bail!(
-            "expected target_proportions to sum to 1.00, got {:}",
-            fund_proportion_sum
-        );
-    }
-
-    let cfg = z3::Config::new();
-    let ctx = Context::new(&cfg);
+fn construct_model<'a>(
+    ctx: &'a Context,
+    funds: &Vec<Fund>,
+    target_buy: f64,
+) -> Option<z3::Model<'a>> {
     let optimize = z3::Optimize::new(&ctx);
 
     let mut vars = Vec::new();
@@ -68,15 +68,15 @@ fn main() -> Result<()> {
     for f in funds.iter() {
         let v = ast::Int::new_const(&ctx, f.symbol.clone());
         let price = f64_to_real(&ctx, f.price);
-        let g = (price * (ast::Real::from_int(&v) + f64_to_real(&ctx, f.shares)))
+        let delta_from_ideal = (price * (ast::Real::from_int(&v) + f64_to_real(&ctx, f.shares)))
             - (&new_total * &f64_to_real(&ctx, f.target_proportion));
-        objective += g
+        objective += delta_from_ideal
             .clone()
             .lt(&ast::Real::from_real(&ctx, 0, 1))
-            .ite(&(-g.clone()), &g.clone());
+            .ite(&(-delta_from_ideal.clone()), &delta_from_ideal.clone());
     }
 
-    let target_buy = &f64_to_real(&ctx, config.target_buy);
+    let target_buy = &f64_to_real(&ctx, target_buy);
     optimize.assert(&total_bought.lt(&target_buy));
 
     // Add penalty for going below the target amount
@@ -84,15 +84,27 @@ fn main() -> Result<()> {
     optimize.minimize(&objective);
 
     optimize.check(&[]);
-    let model = optimize
-        .get_model()
+    optimize.get_model()
+}
+
+fn main() -> Result<()> {
+    let opts: Opts = Opts::parse();
+
+    let config_str = std::fs::read_to_string(opts.config)?;
+    let config: Config = toml::from_str(&config_str)?;
+    config.validate()?;
+    let funds = config.funds;
+
+    let ctx = Context::new(&z3::Config::new());
+
+    let model = construct_model(&ctx, &funds, config.target_buy)
         .ok_or(anyhow!("evaluating model failed"))?;
 
     println!("Optimal purchasing strategy:");
     let mut total = 0.0;
-    for (f, v) in vars {
+    for f in funds {
         let shares = model
-            .eval(&v)
+            .eval(&ast::Int::new_const(&ctx, f.symbol.clone()))
             .and_then(|s| s.as_i64())
             .ok_or(anyhow!("failed to evaluate {}", f.symbol))?;
         let purchase = f.price * (shares as f64);
